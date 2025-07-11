ff35427622971d28d633b2e36f0e8a58e504ddd1
Dominik Chrastecky <dominik@chrastecky.cz>
2025-07-11 20:24:20 +0200

    Feat: Assign groups to user on login

--- app/controllers/redmine_oauth_controller.rb
+++ app/controllers/redmine_oauth_controller.rb
@@ -190,6 +190,7 @@ class RedmineOauthController < AccountController
     raise StandardError, l(:oauth_no_verified_email) unless email
 
     # Roles
+    non_default_roles = []
     keys = RedmineOauth.validate_user_roles.split('.')
     if keys&.size&.positive?
       roles = user_info
@@ -209,11 +210,13 @@ class RedmineOauthController < AccountController
         invalid_credentials
         raise StandardError, l(:notice_account_invalid_credentials)
       end
+
+      non_default_roles = roles - %w[admin user]
     end
 
     # Try to log in
     set_params
-    try_to_login email, user_info
+    try_to_login email, user_info, non_default_roles
     session[:oauth_login] = true
   rescue StandardError => e
     Rails.logger.error e.message
@@ -253,7 +256,7 @@ class RedmineOauthController < AccountController
     session.delete :oauth_autologin
   end
 
-  def try_to_login(email, info)
+  def try_to_login(email, info, role_names)
     user = User.joins(:email_addresses).where(email_addresses: { address: email }).first
     if user # Existing user
       if user.registered? # Registered
@@ -308,6 +311,16 @@ class RedmineOauthController < AccountController
       invalid_credentials
       raise StandardError, l(:notice_account_invalid_credentials)
     end
+
+    role_names.each do |role_name|
+      group = Group.find_by(lastname: role_name)
+      if group
+        unless user.groups.exists?(group.id)
+          user.groups << group
+        end
+      end
+    end
+
     return if @admin.nil?
 
     user = User.find(user.id)
